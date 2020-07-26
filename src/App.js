e2af67289c97d388dbb9a9acd02a3880cde5dd9f
Sonali Najera <sonalitzul@gmail.com>
2020-07-25 20:35:25 -0700

    initial commmit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import AddBookmark from './AddBookmark/addBookmark'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AddBookmark />
       </div>
     );
   }
