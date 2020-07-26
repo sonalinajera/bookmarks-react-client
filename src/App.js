44b9d813544c5d5b240d3cb668845bca77c23d4a
Sonali Najera <sonalitzul@gmail.com>
2020-07-26 15:25:04 -0700

    functional react app

--- src/App.js
+++ src/App.js
@@ -4,28 +4,72 @@ import './App.css';
 import AddBookmark from './addBookmark/addBookmark';
 import BookmarkApp from './bookmarkApp/bookmarkApp';
 
-
-const bookmarks = [
-  {
-  title:"Google",
-  url:"http://www.google.com", 
-  rating:"3", 
-  description:"No evil"
-  },
-  {
-    title:"Google",
-    url:"http://www.google.com", 
-    rating:"3", 
-    description:"No evil"
-  }
-];
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: [],
+      showAddForm: false
+    };
+  }
+
+  componentDidMount() {
+    const url = 'http://localhost:3000/v3/bookmarks';
+    const options = {
+      method: 'GET',
+      headers: {
+        "Authorization": "Bearer $2a$10$rhX3SP.kMB.md22yGSBbxOEA1g5nwJ63sFsozRVKlOwWA5EIq1YkC",
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch(url, options)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong, please try again later.');
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          bookmarks: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+  }
+
+  setShowAddForm(show) {
+    this.setState({
+      showAddForm: show
+    })
+  }
+
+  addBookmark(bookmark) {
+    this.setState({
+      bookmarks: [...this.state.bookmarks, bookmark],
+      showAddForm: false
+    })
+  }
+
   render() {
+    const page = this.state.showAddForm
+          ? <AddBookmark 
+              showForm={show => this.setShowAddForm(show)}
+              handleAdd={bookmark => this.addBookmark(bookmark)}/>
+          : <BookmarkApp 
+              bookmarks={this.state.bookmarks}
+              showForm={show => this.setShowAddForm(show)}/>; 
+
     return (
       <div className="App">
-        <AddBookmark />
-        <BookmarkApp bookmarks={bookmarks}/>
-        
+        { page }
       </div>
     );
   }
